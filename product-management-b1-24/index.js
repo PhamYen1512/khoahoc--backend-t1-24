cd1946cff2880d16eeb77cfc0b020eac50310d5a
Yn <phamyenkdhy11@gmail.com>
2024-07-12 16:19:06 +0700

    Cau hinh folder views

--- product-management-b1-24/index.js
+++ product-management-b1-24/index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+
 app.get("/", (req, res) => {
-    res.send("Trang chu");
+    res.render("client/pages/home/index");
 });
 
 app.get("/products", (req, res) => {
-    res.send("Danh sach san pham");
+    res.render("client/pages/products/index");
 });
 
 app.listen(port, () => {
